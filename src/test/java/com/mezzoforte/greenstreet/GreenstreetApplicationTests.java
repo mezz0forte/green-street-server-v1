e1c7593b0caea83605b84f5ed6477c47ac6f7a33
whitebear05 <woojs0505@naver.com>
2022-07-22 20:51:04 +0900

    :tada: :: start project

---
+++ src/test/java/com/mezzoforte/greenstreet/GreenstreetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mezzoforte.greenstreet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenstreetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
